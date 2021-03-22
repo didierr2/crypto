79e994f2ea3de681107356b2b0aca134870b4ed9
Didier <Didier@host.docker.internal>
2021-03-23 00:49:11 +0100

    Externalisation des constantes dans un properties

--- src/main/java/fr/crypto/shareprice/Constants.java
+++ src/main/java/fr/crypto/shareprice/Constants.java
@@ -1,29 +1,43 @@
 package fr.crypto.shareprice;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
 
+import fr.crypto.shareprice.importer.CoinMarketCapImporter;
+import fr.crypto.shareprice.importer.CoursCryptomonnaiesImporter;
+import fr.crypto.shareprice.importer.SharePriceImportable;
+import fr.crypto.shareprice.util.PropertyManager;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constants {
 
-	public static final SimpleDateFormat SDF_EXCEL = new SimpleDateFormat("dd/MM/yyyy");
+	public static final SimpleDateFormat SDF_EXCEL = new SimpleDateFormat(PropertyManager.getPropertyOrElse("UPDATE_DATE_FORMAT", "dd/MM/yyyy"));
 
-	public static final int FIRST_ROW = 1;
+	public static final int FIRST_ROW = PropertyManager.getIntPropertyOrElse("FIRST_ROW", 1);
 	
-	public static final int SLEEP_INTERVAL_SECONDS = 2;
+	public static final int SLEEP_INTERVAL_MILLISECONDS = PropertyManager.getIntPropertyOrElse("SLEEP_INTERVAL_MILLISECONDS", 1000);
+	
+
+	/** Liste des importers de cours */
+	public static final List<SharePriceImportable> SHARE_PRICE_IMPORTERS = Arrays.asList(new SharePriceImportable[] {
+			new CoursCryptomonnaiesImporter(),
+			new CoinMarketCapImporter()
+	}); 
 	
 	
 	public static final String URL_CRYPTOAST = "/cryptoast.fr/";
 	public static final String URL_COINMARKETCAP = "/coinmarketcap.com/";
+	public static final String URL_COURSCRYPTOMONNAIES = "/courscryptomonnaies.com/";
 	
 	public enum SHEETS {
 		DATA(0);
 		
 		public int value = 0;
 		SHEETS(int n) {
-			value = n;
+			value = PropertyManager.getIntPropertyOrElse(this.getClass().getSimpleName() + "." + this.name(), n);
 		}
 	}
 	
@@ -43,7 +57,7 @@ public final class Constants {
 		
 		public int value = 0;
 		COLS(int n) {
-			value = n;
+			value = PropertyManager.getIntPropertyOrElse(this.getClass().getSimpleName() + "." + this.name(), n);
 		}
 	}
 			
